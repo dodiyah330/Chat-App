3f84305f43524c22b92aba295d91d5a99caef48e
Hitesh Dodiya <87763962+dodiyah330@users.noreply.github.com>
2025-05-20 17:40:38 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
       setShowChat(true);
     }
   };
-  console.log("show Chat heereee!")
+  console.log("show Chat heeree!")
   return (
     <div className="App">
       {!showChat ? (
